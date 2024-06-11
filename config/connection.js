3ddd30f04e0ada13ed764561aa492fabfcfa8ddf
mkatile <mariamkatile06@gmail.com>
2024-06-11 16:21:59 -0400

    commit code

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432
-  });
-}
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', typeof process.env.DB_PASSWORD); // Should print 'string'
+console.log('DB_URL:', process.env.DB_URL);
 
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+      logging: console.log, // Optional: Enable logging for debugging
+    });
 
 module.exports = sequelize;
+
