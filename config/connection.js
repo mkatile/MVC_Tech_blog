83e2ad59e2ff808e8199f6044ce2d65c37c58ea3
mkatile <mariamkatile06@gmail.com>
2024-06-10 21:52:57 -0400

    web app

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: 'localhost',
+    dialect: 'postgresql',
+    port: 5432
+  });
+}
+
+module.exports = sequelize;
